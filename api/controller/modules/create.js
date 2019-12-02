eb0d7c400d8b2c492631f18fa65490f059514d35
Yol-B <yoltorres24@gmail.com>
2019-12-02 13:54:14 +0800

    need retrieve Post

--- api/controller/modules/create.js
+++ api/controller/modules/create.js
@@ -1,9 +1,19 @@
 let models = require("../../model/models");
 module.exports = (reqBody, res) => {
-    req.body.createdAt = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
+    reqBody.user = mongooseTypes.ObjectId(reqBody.user);
+    reqBody.createdAt = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
     let post = new models.Post(reqBody);
+    console.log(post);
     post.save()
         .then(result => {
+            models.Post.findByIdAndUpdate({ _id: reqBody.user }, { $push: { posts: result._id } },
+                (error, success) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log(success);
+                    }
+                })
             res.status(200).send({ error: false, success: true, data: result })
         })
         .catch(err => {

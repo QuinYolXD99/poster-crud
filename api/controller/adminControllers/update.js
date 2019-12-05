683cf99fbfe08fbe9daaf33849430854bda6a511
Yol Torres <54098157+Yol-24@users.noreply.github.com>
2019-12-05 21:52:47 +0800

    Update update.js

--- api/controller/adminControllers/update.js
+++ api/controller/adminControllers/update.js
@@ -1,18 +1,20 @@
 let models = require("../../model/models");
 let timestamp = require("./timestamp")
+const bcrypt = require("bcryptjs");
 const update = (reqBody, res) => {
-    console.log(reqBody);
-    
-    // models.Post.findByIdAndUpdate(reqBody._id, reqBody.post, { new: true }, (err, doc) => {
-    //     console.log(reqBody.post);
-    //     // Handle any possible database errors
-    //     if (err) {
-    //         console.log("error");
-    //         res.status(200).send({ error: true, success: false, data: null })
-    //     } else {
-    //         console.log("success");
-    //         res.status(200).send({ error: false, success: true, data: doc })
-    //     }
-    // })
+    if(reqBody.account.new_password){
+        reqBody.account.password =  bcrypt.hashSync(reqBody.account.new_password, 10);
+    }
+    del reqBody.account.new_password;
+    models.Post.findByIdAndUpdate(reqBody._id,$set:{account:reqBody.account}.post, { new: true }, (err, saved) => {
+        // Handle any possible database errors
+        if (err) {
+            console.log("error");
+            res.status(200).send({ error: true, success: false, data: null })
+        } else {
+            console.log("success");
+            res.status(200).send({ error: false, success: true, admin: saved })
+        }
+    })
 }
 module.exports = update
